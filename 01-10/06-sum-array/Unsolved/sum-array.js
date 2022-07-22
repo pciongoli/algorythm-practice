a3efe852cf61be08a9e1c1643c259d5cc353cae4
pcion <patrick.ciongoli@gmail.com>
2022-07-22 12:42:39 -0400

    completed 06-sum-array

--- 01-10/06-sum-array/Unsolved/sum-array.js
+++ 01-10/06-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,22 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+// [ 3, 1, 5, 6 ]
+// need variable to store sum
+// need to loop through array
+// while looping through array, add that number to sum variable
+// return the sum
+
+var sumArray = function (arr) {
+   var sum = 0;
+
+   for (var i = 0; i < arr.length; i++) {
+      sum += arr[i];
+   }
+   // return sum outside of loop
+   return sum;
+};
+
+// test in browser
+var numberArray = [3, 1, 5, 6];
+
+console.log(sumArray(numberArray));
