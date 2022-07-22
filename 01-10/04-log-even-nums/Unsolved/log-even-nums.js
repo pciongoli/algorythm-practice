b895ac8e8a72ba0902e6aa611f986c17ec20e0a5
pcion <patrick.ciongoli@gmail.com>
2022-07-22 12:55:40 -0400

    completed 04-log-even-nums

--- 01-10/04-log-even-nums/Unsolved/log-even-nums.js
+++ 01-10/04-log-even-nums/Unsolved/log-even-nums.js
@@ -1,4 +1,20 @@
 // Write code to print all even numbers from 0 to `num`
 // Assume `num` will be a positive number
 
-var logEvenNums = function(num) {};
+// need to print even numbers from 0 given the up to a given num argument
+
+// var logEvenNums = function (num) {
+//    for (var i = 0; i <= num; i++) {
+//       // only console log even numbers
+//       if (i % 2 === 0) {
+//          console.log(i);
+//       }
+//    }
+// };
+
+// Alternate solution without the use of if statement
+var logEvenNums = function (num) {
+   for (var i = 0; i <= num; i += 2) {
+      console.log(i);
+   }
+};
