81fbc3712d5a435668a05cfb3c075db6525e06e9
pcion <patrick.ciongoli@gmail.com>
2022-07-22 11:31:22 -0400

    completed 03-log-nums

--- 01-10/03-log-nums/Unsolved/log-nums.js
+++ 01-10/03-log-nums/Unsolved/log-nums.js
@@ -1,4 +1,14 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function (num) {
+   for (var i = 1; i <= num; i++) {
+      console.log(i);
+   }
+};
+
+// for loop
+// 3 statements.
+// Statement 1: sets variable prior to loop starting ( let i = 1)
+// Statement 2: define condition for loop to run (i must be less than or equal to num)
+// Statement 3: what will be executed after statement 2 is satisfied (i++ (add increment of 1 each time))
