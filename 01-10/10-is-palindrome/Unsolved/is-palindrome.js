34258bea5ead1a47c37aa9ab8f691dee8e5f50ac
pcion <patrick.ciongoli@gmail.com>
2022-07-23 10:28:25 -0400

    completed 10-palindrome

--- 01-10/10-is-palindrome/Unsolved/is-palindrome.js
+++ 01-10/10-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,15 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
+// write code that will go through each string
+// if string is a palindrome return true
+// if string is not a plaindrome return false
 
-var isPalindrome = function(str) {};
+var isPalindrome = function (str) {
+   var reversedString = str.split("").reverse().join("");
+
+   // if/else statement returning result of palindrome
+   if (reversedString === str) {
+      return true;
+   } else {
+      return false;
+   }
+};
