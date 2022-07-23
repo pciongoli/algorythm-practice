85795c842c1fdad2b3ab3c6d7996270728af27a1
pcion <patrick.ciongoli@gmail.com>
2022-07-23 10:01:15 -0400

    completed 08-max-number

--- 01-10/08-max-num/Unsolved/max-num.js
+++ 01-10/08-max-num/Unsolved/max-num.js
@@ -1,3 +1,14 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function (arr) {
+   var max = arr[0];
+
+   for (var i = 0; i < arr.length; i++) {
+      var currentNum = arr[i];
+
+      if (currentNum > max) {
+         max = currentNum;
+      }
+   }
+   return max;
+};
