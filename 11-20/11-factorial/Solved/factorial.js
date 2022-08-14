e16e95916540c4b163cee73b05f4b1d74a162825
pcion <patrick.ciongoli@gmail.com>
2022-08-14 14:50:49 -0700

    completed 11-factorial

--- 11-20/11-factorial/Solved/factorial.js
+++ 11-20/11-factorial/Solved/factorial.js
@@ -1,11 +1,11 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {
-  var result = 1;
+var factorial = function (num) {
+   var result = 1;
 
-  for (var i = num; i > 1; i--) {
-    result = result * i;
-  }
+   for (var i = num; i > 1; i--) {
+      result = result * i;
+   }
 
-  return result;
+   return result;
 };
--- 11-20/11-factorial/Unsolved/factorial.js
+++ 11-20/11-factorial/Unsolved/factorial.js
@@ -1,3 +1,13 @@
 // Write code to create a function that returns the factorial of `num`
+// factorial is product of all positive integers less than or equal to number
+// 3 would be (1*2*3=6)
 
-var factorial = function(num) {};
+var factorial = function (num) {
+   var result = 1;
+
+   for (var i = num; i > 1; i--) {
+      result = result * i;
+   }
+
+   return result;
+};
