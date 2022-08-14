35dced299c052b0fbf6a2b995304feeefaee4ed5
pcion <patrick.ciongoli@gmail.com>
2022-08-14 15:29:22 -0700

    completed 15-camel-Case

--- 11-20/15-camel-case/Unsolved/camel-case.js
+++ 11-20/15-camel-case/Unsolved/camel-case.js
@@ -1,3 +1,23 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
+// need to have first word lowercase
+// all remaining words must be combined with first letter uppercase
 
-var camelCase = function(str) {};
+var camelCase = function (str) {
+   var result = "";
+   var words = str.toLowerCase().split(" ");
+
+   for (var i = 0; i < words.length; i++) {
+      var word = words[i];
+      var letters = word.split("");
+
+      if (i > 0) {
+         // set first letter in remaining words to upperCase
+         letters[0] = letters[0].toUpperCase();
+      }
+
+      // use addition assignment += to joint words
+      result += letters.join("");
+   }
+
+   return result;
+};
