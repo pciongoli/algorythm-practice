f4575a80a26741fd454c59f68068c7ca91cddf7d
pcion <patrick.ciongoli@gmail.com>
2022-08-14 15:03:10 -0700

    completed 12-title-case

--- 11-20/12-title-case/Solved/title-case.js
+++ 11-20/12-title-case/Solved/title-case.js
@@ -1,17 +1,17 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {
-  var result = [];
+var titleCase = function (str) {
+   var result = [];
 
-  var words = str.split(' ');
+   var words = str.split(" ");
 
-  for (var i = 0; i < words.length; i++) {
-    var word = words[i].split('');
+   for (var i = 0; i < words.length; i++) {
+      var word = words[i].split("");
 
-    word[0] = word[0].toUpperCase();
+      word[0] = word[0].toUpperCase();
 
-    result.push(word.join(''));
-  }
+      result.push(word.join(""));
+   }
 
-  return result.join(' ');
+   return result.join(" ");
 };
