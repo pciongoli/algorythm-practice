187c1596ee83a13ee2adf978e0f78eac039df2d8
pcion <patrick.ciongoli@gmail.com>
2022-08-16 22:47:59 -0700

    completed 18-is-anogram

--- 11-20/18-is-anagram/Unsolved/is-anagram.js
+++ 11-20/18-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,31 @@
 // TODO: Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
+// automatically rule out (return false) strings that are not the same length
+// need to compare strings that are the same length
+// if anogram return true
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function (strA, strB) {
+   if (strA.length !== strB.length) {
+      return false;
+   }
+
+   var aLetterMap = {};
+   var bLetterMap = {};
+   var strLength = strA.length;
+
+   for (var i = 0; i < strLength; i++) {
+      var aLetter = strA[i];
+      var bLetter = strB[i];
+
+      aLetterMap[aLetter] = (aLetterMap[aLetter] || 0) + 1;
+      bLetterMap[bLetter] = (bLetterMap[bLetter] || 0) + 1;
+   }
+
+   for (var key in aLetterMap) {
+      if (aLetterMap[key] !== bLetterMap[key]) {
+         return false;
+      }
+   }
+
+   return true;
+};
