2b12d1f5facec493d3d0dae421f423976de203cd
pcion <patrick.ciongoli@gmail.com>
2022-08-19 15:39:23 -0700

    completed 20-is-perfect-square

--- 11-20/20-is-perfect-square/Solved/is-perfect-square.js
+++ 11-20/20-is-perfect-square/Solved/is-perfect-square.js
@@ -1,18 +1,20 @@
 // Write code to create a function that accepts a number and returns true if the number is a perfect square, otherwise it returns false.
 // You may not use the built-in Math.sqrt method
 
-var isPerfectSquare = function(num) {
-  var i = 0;
+var isPerfectSquare = function (num) {
+   var i = 0;
 
-  while (true) {
-    var currentSquare = i * i;
+   while (true) {
+      var currentSquare = i * i;
 
-    if (currentSquare === num) {
-      return true;
-    } else if (currentSquare > num) {
-      return false;
-    } else {
-      i++;
-    }
-  }
+      // when currentSquare is equal to num return true
+      if (currentSquare === num) {
+         return true;
+         // if not equal return false
+      } else if (currentSquare > num) {
+         return false;
+      } else {
+         i++;
+      }
+   }
 };
--- 11-20/20-is-perfect-square/Unsolved/is-perfect-square.js
+++ 11-20/20-is-perfect-square/Unsolved/is-perfect-square.js
@@ -1,4 +1,19 @@
 // Write code to create a function that accepts a number and returns true if the number is a perfect square, otherwise it returns false.
 // You may not use the built-in Math.sqrt method
 
-var isPerfectSquare = function(num) {};
+var isPerfectSquare = function (num) {
+   var i = 0;
+
+   while (true) {
+      var currentSquare = i * i;
+      // return true if currentSquare equals num
+      if (currentSquare === num) {
+         return true;
+         // else if return false
+      } else if (currentSquare > num) {
+         return false;
+      } else {
+         i++;
+      }
+   }
+};
