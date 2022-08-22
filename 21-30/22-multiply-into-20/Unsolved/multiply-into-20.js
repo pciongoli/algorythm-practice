d6b0dcd8d9a160f0cd2ab939828f60ba87289d17
pcion <patrick.ciongoli@gmail.com>
2022-08-22 13:48:09 -0700

    completed 22-multiply-into-20

--- 21-30/22-multiply-into-20/Unsolved/multiply-into-20.js
+++ 21-30/22-multiply-into-20/Unsolved/multiply-into-20.js
@@ -1,3 +1,24 @@
-const multiplyInto20 = function(arr) {
-  // TODO: Write Your Code Here
+// need to write code that will take array of unique numbers
+// return true if product of any two numbers is 20
+// if else return false
+
+// set constant to empty object
+//  need a loop to cycle through current number in array
+// if statement to check product and return true if = 20
+// return false if product anything else
+
+const multiplyInto20 = function (arr) {
+   const numMap = {};
+
+   for (let i = 0; i < arr.length; i++) {
+      let currentNum = arr[i];
+
+      let neededNumber = 20 / currentNum;
+
+      if (numMap[neededNumber]) {
+         return true;
+      }
+      numMap[currentNum] = true;
+   }
+   return false;
 };
